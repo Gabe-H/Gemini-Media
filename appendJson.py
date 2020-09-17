e09dc25ff15b237598578e15b12a05c668413517
Gabe-H <gabe.haarberg@gmail.com>
2020-09-16 22:34:03 -0700

    update

--- appendJson.py
+++ appendJson.py
@@ -4,11 +4,12 @@ with open('videos.json') as f:
     data = json.load(f)
 
 myID = input('Spotify URI: ')
+myID = myID.split(':')
+myID = myID[len(myID)-1]
+print(myID)
 myURL = input('URL: ')
 myCSS = input('CSS (leave blank if none): ')
 
-myID = myID.split(':')
-myID = myID[len(myID)-1]
 
 data['ids'].append({
     "id": myID,
